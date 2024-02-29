47295e2b3ba74c71d2d45468df5790aa8bc2c00f
tarasmoroz <taras.moroz@ekreative.com>
2024-02-29 14:50:35 +0100

    Added functional for remove dir or file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ fs.readFile('./test.txt','utf8', (error, data) => {
     console.log(data);
 });
 
-console.log('Just text!');
+// console.log('Just text!');
 
 // Write file
 fs.readFile('./test.txt','utf8', (error, data) => {
@@ -17,3 +17,23 @@ fs.readFile('./test.txt','utf8', (error, data) => {
         error ? console.log(error):null;
     });
 });
+
+// Remove file
+setTimeout(() => {
+    // Check exist file
+    if (fs.existsSync("./files/test2.txt")) {
+        fs.unlink("./Files/test2.txt", () => {
+            console.log('file was removed')
+        });
+    }
+}, 4000);
+
+// Remove dir
+setTimeout(() => {
+    // Check exist dir
+    if (fs.existsSync("./files")) {
+        fs.rmdir("./files", () => {
+            console.log('Dir was removed')
+        });
+    }
+}, 6000);
