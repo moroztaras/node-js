84312232b1cdfe81cd6f57d21520a2e4f36cd781
tarasmoroz <taras.moroz@ekreative.com>
2024-02-29 13:25:35 +0100

    import & export variables and functions

--- modules.js
+++ modules.js
@@ -1,3 +1,20 @@
-// Import data form module
-const user = require('./test.js');
-console.log(user)
+// Import variable form module
+// const user = require('./test.js');
+// console.log(user)
+
+// Import variable and function form module
+const {userName: user, sayHi} = require('./test.js');
+
+console.log(user);
+
+const name = "Tommy";
+
+console.log(sayHi(name));
+
+// Export variable from this module
+module.exports = name;
+
+// Import module OS
+const os = require("os");
+// Show info about platform and release
+console.log(os.platform(), os.release());
--- test.js
+++ test.js
@@ -3,4 +3,10 @@ const userName = "Taras";
 const sayHi = (userName) => `Hello, my name is ${userName}`;
 
 // Make module for export only variable
-module.exports = userName;
+// module.exports = userName;
+
+// Make module for export object (variable and function)
+module.exports = {
+    userName,
+    sayHi,
+};
